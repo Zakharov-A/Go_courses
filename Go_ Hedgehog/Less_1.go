52929b55a698f238e1472c7a25ed415178212337
Zakharov-A <redzed427@gmail.com>
2022-02-12 12:36:15 -0500

    Hedgehog_less_1_and_2

---
+++ Go_ Hedgehog/Less_1.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+
+  var h int8 = 2
+  var g float64 = float64(h)
+  fmt.Println(g)
+
+
+
+  // var name string
+  // var age uint8
+  // fmt.Println("Whot is your name? ")
+  // fmt.Scan(&name)
+  // fmt.Println("Hello " + name + "!")
+  //
+  // fmt.Println("How old are you?")
+  // fmt.Scan(&age)
+  // fmt.Println("You are " + fmt.Sprint(age) + " years!")
+
+
+
+  // var age uint8 = 23
+  // fmt.Println(age)
+
+
+}
---
+++ Go_SQL/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+  "fmt"
+  "database/sql"
+		_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+  fmt.Println("Работа с MySQL")
+}
