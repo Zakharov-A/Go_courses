e2ffcf32f0e17d3a41955d6b69b5d72ce187e2df
Zakharov-A <redzed427@gmail.com>
2022-02-21 07:15:56 -0500

    Go_DTA_Simple_less_5_array_slice

--- Digital_Technology_Agency_course/simple/Less_5_arrays.go
+++ Digital_Technology_Agency_course/simple/Less_5_arrays.go
@@ -12,17 +12,37 @@ import "fmt"
 // }
 
 func main() {
-	var x_exam [5]float64
-	x_exam[0] = 98
-	x_exam[1] = 93
-	x_exam[2] = 77
-	x_exam[3] = 82
-	x_exam[4] = 83
+	//Объявление массива
+	// var x_exam [5]float64
+	// x_exam[0] = 98
+	// x_exam[1] = 93
+	// x_exam[2] = 77
+	// x_exam[3] = 82
+	// x_exam[4] = 83
+	//Короткое объявление массива
+	// x_exam := [6]float64{98, 93, 77, 82, 83, 99}
+	// x_exam := [6]float64{
+	// 	98,
+	// 	93,
+	// 	77,
+	// 	82,
+	// 	83,
+	// 	99,
+	// }
 
-	var tot float64
-	for i := 0; i < len(x_exam); i++ {
-		tot += x_exam[i]
-	}
-	fmt.Println(tot / float64(len(x_exam)))
+	// var tot float64 = 0
+	// for _, value := range x_exam {
+	// 	tot += value
+	// }
+	// fmt.Println(tot / float64(len(x_exam)))
+
+	// Срезы
+	// var xlist []float64
+	// x := make([]float64, 5, 10)
+	// fmt.Println(xlist, x)
+
+	arr := [5]float64{1, 2, 3, 4, 5}
+	x := arr[0:5]
+	fmt.Println(x)
 
 }
