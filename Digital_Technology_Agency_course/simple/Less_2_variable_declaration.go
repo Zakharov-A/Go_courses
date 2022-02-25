5a8f4028e1cb5306367095130cb63f9371c5eacf
Zakharov-A <global user.email redzed427@gmail.com>
2022-02-25 17:46:11 +0300

    Go_simple

---
+++ Digital_Technology_Agency_course/simple/Less_2_variable_declaration.go
@@ -0,0 +1,75 @@
+package main
+
+import "fmt"
+
+// ---- global variable ----
+// var First = "Good Word" //Глобал
+
+// var first = "word" // Переменная с маленькой буквы внутри пакета
+
+// ---- String output ----
+// func main() {
+// 	first := "Roghan the best!"
+// 	Run(first)
+//
+//
+//
+// }
+//
+// func Run(xxx string) {
+// 	fmt.Printf("%s", xxx)
+// }
+
+// ----
+
+// ---- Hello ----
+// func main() {
+//     var x string
+//     x = "Hello World"
+//     fmt.Println(x)
+// }
+
+// ----
+
+// ---- "Addition of strings" ----
+// func main() {
+// 	var x string
+// x = "first "
+// fmt.Println(x)
+// x = x + "second"
+// fmt.Println(x)
+//
+// }
+
+// ----
+
+// ---- constant declaration ----
+// func main() {
+//     const x string = "Hello World"
+//     fmt.Println(x)
+// }
+
+// ---- Defining Multiple Variables ----
+// func main() {
+// 	var (
+//     a = 5
+//     b = 10
+//     c = 15
+// )
+//
+// fmt.Println(a, b, c)
+// }
+
+// ---- Program example ----
+
+func main() {
+    fmt.Print("Enter a number: ")
+    var input float64
+    fmt.Scanf("%f", &input)
+
+    output := input * 2
+
+    fmt.Println(output)
+	}
+
+// ----
