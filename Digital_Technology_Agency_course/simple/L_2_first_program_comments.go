fd68fd9b5190a0168e0822b9b95874b9154d8455
Zakharov-A <redzed427@gmail.com>
2022-03-01 08:31:12 -0500

    GO_hash_table_comments_l1

---
+++ Digital_Technology_Agency_course/simple/L_2_first_program_comments.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+// ---- first program ----
+
+// func main() {
+// 	var Hi_You = "Hi World"
+// 	fmt.Printf("String: %s", Hi_You)
+// }
+
+// ----
+
+// ---- ----
+
+func main() {
+	var Hi_You = "Hi World!"
+	Run(Hi_You)
+}
+
+func Run(Hi_You string) {
+	fmt.Printf("%s", Hi_You)
+}
+
+// ----
