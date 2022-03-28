8ecc3bcb60fe53202580e1c4082a992f79c3f318
Zakharov-A <redzed427@gmail.com>
2022-03-28 08:14:03 -0400

    GO_course_DTA_L_21_struct_methods

---
+++ Digital_Technology_Agency_course/simple/L_21_struct_methods.go
@@ -0,0 +1,38 @@
+package main
+
+// ---- Struct Methods ----
+
+type Cpu struct {
+	Prod string
+	Core int
+	Hard int
+}
+
+type Pc struct {
+	Cpu
+}
+
+func (pc Pc) On() {
+	println("[On] Shutdown PC")
+}
+
+func (pc Pc) Off() {
+	println("[Off] Turning on PC")
+}
+
+func (pc Pc) initMethod() {
+	println("[Init] ... ")
+}
+
+func main() {
+	var p Pc = Pc{
+		Cpu: Cpu{
+			Prod: "Intel",
+			Core: 8,
+		},
+	}
+	p.On()
+	p.Off()
+}
+
+// ----
