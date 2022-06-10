8271e88235636e94060a10b35654c194e7aa3d25
Darth <redzed427@gmail.com>
2022-06-10 03:30:11 -0400

    Go_course_ThisIsIT_lesson_3_part_2_functions

--- Golang_basics/lesson_3/function.go
+++ Golang_basics/lesson_3/function.go
@@ -12,13 +12,17 @@ func funcName(params...) (returned values) {
 */
 
 func main() {
-	first, second := 2, 2
+	first, second, third := 3, 2, 6
+	first1, second1 := 23, 45
 	Greet()
 	PersonGreet("Sam")
 	FioGreet("John", "Smith")
 	sum := Sum(first, second)
 	fmt.Println(sum)
-
+	summa, multiply, subtraction := SumAndMultiply(first, second, third)
+	fmt.Println(summa, multiply, subtraction)
+	_, multiply64 := namedSumAndMultiply(first1, second1)
+	fmt.Println(multiply64)
 }
 
 func Greet() {
@@ -37,4 +41,20 @@ func FioGreet(name, surname string) {
 func Sum(first, second int) int {
 	sum := first * second
 	return sum
+}
+
+func SumAndMultiply(first, second, third int) (int, int, int) {
+	return first + second, first * second, third - second
+}
+
+// func namedSumAndMultiply(first, second int) (sum1 int64, multiply1 int64) {
+// 	sum1 = int64(first + second)
+// 	multiply1 = int64(first) * int64(second)
+// 	return
+// }
+
+func namedSumAndMultiply(first, second int) (sum1 int64, multiply1 int64) {
+	sum1 = int64(first + second)
+	multiply1 = int64(first) * int64(second)
+	return sum1, multiply1
 }
