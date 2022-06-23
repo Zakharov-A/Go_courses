ddd89ee12af834b4771b7a9c9c1485981789eb06
Darth <redzed427@gmail.com>
2022-06-23 18:53:32 -0400

    ThisIsIT_course_lesson_8_9_parts_2

--- Golang_basics/lesson_7/cycles_continue_break.go
+++ Golang_basics/lesson_7/cycles_continue_break.go
@@ -5,28 +5,28 @@ import "fmt"
 func main() {
 	fmt.Println("Hi")
 
-	for i := 0; i <= 20; i++ {
-		if i%2 == 1 {
-			continue
-		}
-		// fmt.Println(i)
-	}
+	// for i := 0; i <= 20; i++ {
+	// 	if i%2 == 1 {
+	// 		continue
+	// 	}
+	// 	// fmt.Println(i)
+	// }
 
-label1:
-	for i := 1; i <= 20; i++ {
-		for j := 1; j <= 10; j++ {
-			// fmt.Println("I:", i, "j", j)
-			if i >= 10 {
-				continue label1
-			}
-		}
-	}
+	// label1:
+	// 	for i := 1; i <= 20; i++ {
+	// 		for j := 1; j <= 10; j++ {
+	// 			fmt.Println("I:", i, "j", j)
+	// 			if i >= 10 {
+	// 				// continue label1
+	// 			}
+	// 		}
+	// 	}
 
 	for i := 0; i <= 20; i++ {
 		if i >= 10 {
 			break
 		}
-		fmt.Println(i)
+		// fmt.Println(i)
 	}
 
 label2:
